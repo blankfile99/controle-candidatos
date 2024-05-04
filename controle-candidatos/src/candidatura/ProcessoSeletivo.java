6b8ea9133a5bf5a1e40e1361a26f0f3b74ed7f75
Renato de Castro <blankfile99@gmail.com>
2024-05-03 21:37:40 -0400

    Adição de listas para recebimento de candidatos e impressão das mesmas - aula 3

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,14 +1,28 @@
 package candidatura;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-        selecaoDeCandidatos();
+        List<String> selecionados = selecaoDeCandidatos();
+        imprimirSelecionados(selecionados);
     }
-    static void selecaoDeCandidatos(){
+
+    static void imprimirSelecionados(List<String> selecionados){
+        System.out.println("Imprimindo candidatos Selecionados.");
+        int i = 1;
+        for(String selecionado : selecionados){
+            System.out.println("O candidato de nº"+i+" é "+selecionado);
+            i++;
+        }
+    }
+    static List<String> selecaoDeCandidatos(){
         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+        List <String> selecionados = new ArrayList<>();
         double salarioBase =2000;
 
         int candidatosSelecionados = 0;
@@ -21,10 +35,13 @@ public class ProcessoSeletivo {
 
             if (salarioBase > salarioPretendido) {
                 System.out.println("O candidato " + candidato + " foi selecionado");
+                selecionados.add(candidato);
                 candidatosSelecionados++;
             }
             candidatoAtual++;
         }
+        return selecionados;
+
     }
     static double valorPretendido(){
         return ThreadLocalRandom.current().nextDouble(1800,2200);
